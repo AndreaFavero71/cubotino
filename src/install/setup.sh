a5f7fd4f8b8a958cc1c2d5b6c58844320d3d8f68
Andrea <108091411+AndreaFavero71@users.noreply.github.com>
2022-07-01 20:28:34 +0200

    synced manually with repo from Yannick
    Fixed the setup script for the pizero to bypass the bad python package checksum

--- src/install/setup.sh
+++ src/install/setup.sh
@@ -38,7 +38,7 @@ apt remove -y -qq python3-numpy python3-picamera
 apt autoremove -y
 
 print_header "Installing required packages"
-apt install -y -qq python3-rpi.gpio python3-pigpio python3-gpiozero python3-pil python3-spidev python3-pip python3-venv
+apt install -y -qq python3-rpi.gpio python3-pigpio python3-gpiozero python3-pil python3-pip python3-venv
 apt install -y -qq libatlas-base-dev python3-h5py libjasper-runtime libqtgui4 libqt4-test
 
 print_header "Creating python virtual env"
@@ -48,10 +48,17 @@ source .virtualenvs/bin/activate
 print_header "Installing required python packages"
 pip3 install numpy==1.21.4
 pip3 install "picamera[array]"
-pip3 install opencv-contrib-python==4.1.0.25
 pip3 install st7735==0.0.4.post1
 pip3 install RubikTwoPhase==1.0.9
 pip3 install getmac==0.8.3
+
+# hash for opencv for pizero seems to be bad on pywheel, bypass it for the moment. It is ok for pizero 2w
+machine=$(uname -m)
+if [ "$machine" == "armv6l" ]; then
+pip3 install https://www.piwheels.org/simple/opencv-contrib-python/opencv_contrib_python-4.1.0.25-cp37-cp37m-linux_armv6l.whl
+else
+pip3 install opencv-contrib-python==4.1.0.25
+fi
 
 print_header "Configuring pigpiod to start on boot"
 systemctl enable pigpiod
